1dfcf832e97b21c2b894defd50b27ee7588da812
gaboliveiradev <g38789138@gmail.com>
2022-10-27 13:23:35 -0300

    criando os arquivos principais e suas respectivas rotas

---
+++ App/rotas.php
@@ -0,0 +1,25 @@
+<?php
+    $parse_uri = parse_url($_SERVER['REQUESTE_URI'], PHP_URL_PATH);
+
+    switch($parse_uri) {
+
+        // http://localhost:8000/api/index
+        case "/api/index":
+            
+        break;
+
+        // http://localhost:8000/api/rotas
+        case "/api/rotas":
+
+        break;
+
+        // http://localhost:8000/api/config
+        case "/api/config":
+
+        break;
+
+        // http://localhost:8000/api/autoload
+        case "/api/autoload":
+
+        break;
+    }
