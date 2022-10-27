58b8b6afc4ab1cf9e671778158956d00da9f3bac
gaboliveiradev <g38789138@gmail.com>
2022-10-27 14:42:24 -0300

    buildando os arquivos principais do template

---
+++ App/Template/rotas.php
@@ -0,0 +1,13 @@
+<?php
+
+$parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch($parse_uri) {
+    case "/welcome":
+        
+    break;
+
+    default:
+        header("Location: /welcome");
+    break;
+}
