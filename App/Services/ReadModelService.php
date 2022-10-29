dbd198b1e919b827ee334e166ab86a9cd74a7526
gaboliveiradev <g38789138@gmail.com>
2022-10-28 23:38:29 -0300

    continuação

---
+++ App/Services/ReadModelService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+class ReadModelService {
+
+    public $arr_rows = [];
+
+    public function readModel() {
+        $arq = fopen("./Template/Model/Model.php", "r");
+        $i = 0;
+
+        while(!feof($arq)) {
+            $row = fgets($arq);
+            $arr_rows[$i] = $row;
+            $i++;
+        }
+
+        fclose($arq);
+
+        return json_encode($arr_rows);
+    }
+
+}
