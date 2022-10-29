c6d7cb06f71c94f01cbe5f82bedbb95b86b71b57
gaboliveiradev <g38789138@gmail.com>
2022-10-28 22:58:56 -0300

    build do serviçio readIndex()

---
+++ App/Services/ReadIndexService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+class ReadIndexService {
+
+    public $arr_rows = [];
+
+    public function readIndex() {
+        $arq = fopen("./Template/index.php", "r");
+        $i = 0;
+
+        while(!feof($arq)) {
+            $row = fgets($arq);
+            $arr_rows[$i] = $row;
+            $i++;
+        }
+
+        fclose($arq);
+
+        return json_encode($arr_rows);
+    }
+
+}
